e65a1f720e49726528caad1a76e1ea9c0fdc040d
Marcelo Curti <marcelo_curtivieira@hotmail.com>
2021-04-20 17:34:29 -0300

    cors resolvido

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 console.log("Usando fetch");
 
-fetch('https://backend-marcelo.herokuapp.com/api/answers/1',{
-    method: 'GET'})
-    
+fetch('https://backend-marcelo.herokuapp.com/api/answers/').then(resp => console.log(resp))
+fetch('https://backend-marcelo.herokuapp.com/api/answers/', {
+    method: 'GET'
+})
+
     .then((response) => {
         if (!response.ok) throw new Error("Erro aou executar a requisição" + response.status);
         return response.json();
     })
 
     .then((data) => {
-        console.log("data.nome");
+        console.log(data.nome);
     })
 
     .catch((error) => {
         console.error(error.message);
     });
+
+console.log(response.body);
