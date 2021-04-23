2ad6c04fdc9e983e5cb61f7f05994ba35b4a1882
Marcelo Curti <marcelo_curtivieira@hotmail.com>
2021-04-22 22:08:54 -0300

    fetch ajax feito

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 console.log("Usando fetch");
 
-fetch('https://backend-marcelo.herokuapp.com/api/answers/').then(resp => console.log(resp))
-fetch('https://backend-marcelo.herokuapp.com/api/answers/', {
-    method: 'GET'
-})
-
-    .then((response) => {
-        if (!response.ok) throw new Error("Erro aou executar a requisição" + response.status);
-        return response.json();
-    })
-
-    .then((data) => {
-        console.log(data.nome);
-    })
-
-    .catch((error) => {
-        console.error(error.message);
+fetch('https://backend-marcelo.herokuapp.com/api/answers/')
+    .then(resp => resp.json())
+    .then(resp2 => {
+        console.log({ resp2 });
+        let nomeSalvos = document.getElementById('nomes-salvos');
+        for (let data of resp2.data) {
+            nomeSalvos.innerHTML += `<li>${data.nome}</li>`;
+            console.log(data.nome);
+        }
     });
-
-console.log(response.body);
+// this is the id of the form
+$(document).on('submit', 'form', function (e) {
+    console.log("executando");
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+    var form = $(this);
+    var url = form.attr('action');
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: form.serialize(), // serializes the form's elements.
+        success: function (data) {
+            location.reload();
+            console.log("Certo! Certissimo!"); // show response from the php script.
+        },
+        error: function (error) {
+            alert("Algo de errado não esta certo");
+        }
+    });
+});
